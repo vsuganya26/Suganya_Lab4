c3d53ebcdab1fba893c49aea126fad52f4eb6dd5
vsuganya26 <106871256+vsuganya26@users.noreply.github.com>
2022-08-23 04:28:25 -0700

    Update Stored_procedure.sql

--- Stored_procedure.sql
+++ Stored_procedure.sql
@@ -6,17 +6,17 @@
     BEGIN
     select report.supp_id, report.supp_name, report.average,
     CASE
-		WHEN report.Average =5 THEN 'Excellent Service'
+	WHEN report.Average =5 THEN 'Excellent Service'
         WHEN report.Average >4 THEN 'Good Service'
-		WHEN report.Average >2 THEN 'Average Service'
+	WHEN report.Average >2 THEN 'Average Service'
         ELSE 'Poor Service'
-        END As Type_of_Service from
+    END As Type_of_Service from
         (select final.supp_id, supplier.supp_name, final.Average from
         (select test2.supp_id, sum(test2.rat_ratstars) as Average from 
         (select supplier_pricing.supp_id, test.ORD_ID, test.RAT_RATSTARS from supplier_pricing inner join
         (select `order`.pricing_id, rating.ORD_ID, rating.RAT_RATSTARS from `order`inner join rating on rating.ord_id = `order`.ord_id) as test )
         as test2 group by supplier_pricing.supp_id) as final inner join supplier
         where final.supp_id = supplier.supp_id) as report;
-	END &&
+    END &&
     
     call procedure1();
